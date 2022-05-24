a8be4d001dc0ab6dd93938666ededd8ccc0f3d4b
Gino Mena <gmenasrc@gmail.com>
2022-05-24 18:58:48 -0300

    Ejercicio_Clase_Ejemplo_If_Boolean

--- src/menagino/flujo/Anidamiento_If_clase.java
+++ src/menagino/flujo/Anidamiento_If_clase.java
@@ -6,12 +6,36 @@ public class Anidamiento_If_clase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		
+				
 		int edad = Integer.parseInt(JOptionPane.showInputDialog("¿Que edad tienes?"));
 		
+		boolean mayor_edad=false;
+		
+		if(edad>=18) {
+			mayor_edad=true;
+		}
+		
+		if (mayor_edad){//no es necesario dictaminarla como false, cuando aparece de esta manera si cumple con la condicion entrara satisfactoriamente al condicional
+			
+			JOptionPane.showMessageDialog(null, "Eres mayor de edad");
+			
+			String Libreta=JOptionPane.showInputDialog("¿Tienes Lireta de conducir?");
+			
+			if (Libreta.equalsIgnoreCase("si")) JOptionPane.showMessageDialog(null, "Todos los datos estan correto, puedes conducir");
+												
+			else JOptionPane.showMessageDialog(null, "Eres mayor de edad pero no tienes libreta de conducir por lo contrario no pueder circular");
+														
+		}else JOptionPane.showMessageDialog(null, "Lo siento los menores de edad no pueden conducir");
+								
+	}
+
+}
+
+/*//Codigo Base, arriba implementamos Booleanos y simplificacion del codigo, cuando en un "IF" hay solo una linea de codigo podemos omitir las "{}" lo mismo en el ELSE. 
+ * int edad = Integer.parseInt(JOptionPane.showInputDialog("¿Que edad tienes?"));
+		
 		if (edad>=18){
+			
 			JOptionPane.showMessageDialog(null, "Eres mayor de edad");
 			
 			String Libreta=JOptionPane.showInputDialog("¿Tienes Lireta de conducir?");
@@ -19,14 +43,14 @@ public class Anidamiento_If_clase {
 			if (Libreta.equalsIgnoreCase("si")) {
 				
 				JOptionPane.showMessageDialog(null, "Todos los datos estan correto, puedes conducir");
+				
 			}else {
+				
 				JOptionPane.showMessageDialog(null, "Eres mayor de edad pero no tienes libreta de conducir por lo contrario no pueder circular");
 			}
 			
 		}else {
+			
 			JOptionPane.showMessageDialog(null, "Lo siento los menores de edad no pueden conducir");
-		}
-
-	}
-
-}
+		}*/
+ 

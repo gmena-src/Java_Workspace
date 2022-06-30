5ad95518aa3d6ac705029c5f1f8ac555fa3d1296
Gino Mena <gmenasrc@gmail.com>
2022-06-30 18:04:57 -0300

    Actualizacion

--- src/Ejercicios/UsoCuentas.java
+++ src/Ejercicios/UsoCuentas.java
@@ -5,7 +5,7 @@ public class UsoCuentas {
 	public static void main(String[] args) {
 		
 	
-		/*CuentaCorriente cuenta1=new CuentaCorriente("Juan", 52345.45);
+		CuentaCorriente cuenta1=new CuentaCorriente("Juan", 52345.45);
 		CuentaCorriente cuenta2=new CuentaCorriente("Carlos Perez", 22349.45);
 		CuentaCorriente cuenta3=new CuentaCorriente("Gino", 1000);
 	
@@ -14,7 +14,7 @@ public class UsoCuentas {
 		System.out.println(cuenta3.getDatos());
 		
 		cuenta1.setTransferencia(cuenta1, cuenta2, 200);
-		System.out.println("Saldo cuenta 1 "+cuenta1.getSaldo());*/
+		System.out.println("Saldo cuenta 1 "+cuenta1.getSaldo());
 		
 		CuentaCorriente c4=new CuentaCorriente("", 0); 
 		System.out.println(c4.getDatos());
